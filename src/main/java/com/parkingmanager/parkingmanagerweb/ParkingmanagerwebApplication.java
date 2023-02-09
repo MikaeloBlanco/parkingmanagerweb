8d4b72955790dabf981239ccf365afcf820f4c4a
David Hormigo Ramírez <dhorram948@g.educaand.es>
2023-02-09 12:33:11 +0100

    Creación de proyecto

---
+++ src/main/java/com/parkingmanager/parkingmanagerweb/ParkingmanagerwebApplication.java
@@ -0,0 +1,13 @@
+package com.parkingmanager.parkingmanagerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingmanagerwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingmanagerwebApplication.class, args);
+	}
+
+}
