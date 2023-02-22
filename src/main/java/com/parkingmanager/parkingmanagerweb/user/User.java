e6b170fc921b5c8b8bb2fafb4a9038e1245406aa
Adolfo Burgos <aburbel032@g.educaand.es>
2023-02-22 21:04:53 +0100

    jadsf

--- src/main/java/com/parkingmanager/parkingmanagerweb/user/User.java
+++ src/main/java/com/parkingmanager/parkingmanagerweb/user/User.java
@@ -1,6 +1,14 @@
 package com.parkingmanager.parkingmanagerweb.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
 
     // Propiedades
     private String firstName;
