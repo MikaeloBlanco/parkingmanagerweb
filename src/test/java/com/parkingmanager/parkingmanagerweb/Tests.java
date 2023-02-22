3aebe52139fad7da4375519d9e586fec20e249c0
Miguel <mblafer963@g.educaand.es>
2023-02-22 19:27:41 +0100

    Ampliacion test

--- src/test/java/com/parkingmanager/parkingmanagerweb/Tests.java
+++ src/test/java/com/parkingmanager/parkingmanagerweb/Tests.java
@@ -21,6 +21,34 @@ public class Tests {
         assertEquals(expected, actual);
     }
 
+    public void check_user_lastName1() {
+        User david = new User("David", "Hormigo",  "Ramírez", "Profesor");
+        String expected = "Hormigo";
+
+        String actual = david.getLastName1();
+
+        assertEquals(expected, actual);
+    }
+
+    public void check_user_lastName2() {
+        User david = new User("David", "Hormigo",  "Ramírez", "Profesor");
+        String expected = "Ramírez";
+
+        String actual = david.getLastName2();
+
+        assertEquals(expected, actual);
+    }
+
+    public void check_user_role() {
+        User david = new User("David", "Hormigo",  "Ramírez", "Profesor");
+        String expected = "Profesor";
+
+        String actual = david.getRole();
+
+        assertEquals(expected, actual);
+    }
+
+
     public void check_sorteo_estado(){
 
         Sorteo sorteo1 = new Sorteo("Primer sorteo Junio","19-06-2023","Completado");
